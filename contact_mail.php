0bc4fe39dbee532b861d8c8e7add5faeaa2b702e
Singhaniya <64910283+Singhaniya626@users.noreply.github.com>
2021-02-12 11:45:51 +0530

    Update contact_mail.php

--- contact_mail.php
+++ contact_mail.php
@@ -3,44 +3,66 @@ session_start();
 error_reporting(1);
 extract($_REQUEST);
 
-include ("class.phpmailer.php");
-$mail = new PHPMailer();
-$mail->IsSMTP();
-$mail->Host = "smtp.gmail.com";
-$mail->Port = 587;
-//$mail->SMTPAuth = true;
-$mail->SMTPSecure = "tls";
-$mail->SMTPAuth = true;
-//$mail->Port = 587;
 
-$mail->Username = "shivam@technoraga.com";
-$mail->Password = "technoragaintern123!@#";
-$mail->From = "shivam@technoraga.com";
-$mail->FromName = "CITY DIAGNOSTIC";
-// $mail->addAddress('vetant@technoraga.com');
-$mail->addAddress('contactform21@gmail.com');
-
-$mail->IsHTML(true);
-$mail->Subject = "CITY DIAGNOSTIC";
-$mail->Body = "
-<pre style='font-size:21px'>
-	Name 	:	$name
-	Email 	:	$email
-	Msg 	:	$message
-</pre>
+$to = "contactform21@gmail.com";
+$subject = "Nebun Holiday Contact Page Query";
+$msg =  "
+  Name  : $name
+  Email   : $email
+  Mobile  : $mobile
+  Sub   : $subject
+  Msg   : $msg
 ";
+$headers = "contactform21@gmail.com";
 
-      if (!$mail->send())
-      {
-        //$error = "Mailer Error: " . $mail->ErrorInfo;
-        // echo '<span>'.$error.'</span>';
-        header("location:index.php?msg=no");
-      }
-      else
-      {
-        header("location:index.php?msg=yes");
-        // echo "Send Mail";
-      }
+
+if(mail($to,$subject,$msg,$headers))
+{
+    header("location:index.php?msg=yes");
+}
+else
+{
+    header("location:index.php?msg=no");
+}
+
+// include ("class.phpmailer.php");
+// $mail = new PHPMailer();
+// $mail->IsSMTP();
+// $mail->Host = "smtp.gmail.com";
+// $mail->Port = 587;
+// //$mail->SMTPAuth = true;
+// $mail->SMTPSecure = "tls";
+// $mail->SMTPAuth = true;
+// //$mail->Port = 587;
+
+// $mail->Username = "shivam@technoraga.com";
+// $mail->Password = "technoragaintern123!@#";
+// $mail->From = "shivam@technoraga.com";
+// $mail->FromName = "CITY DIAGNOSTIC";
+// // $mail->addAddress('vetant@technoraga.com');
+// $mail->addAddress('contactform21@gmail.com');
+
+// $mail->IsHTML(true);
+// $mail->Subject = "CITY DIAGNOSTIC";
+// $mail->Body = "
+// <pre style='font-size:21px'>
+// 	Name 	:	$name
+// 	Email 	:	$email
+// 	Msg 	:	$message
+// </pre>
+// ";
+
+//       if (!$mail->send())
+//       {
+//         //$error = "Mailer Error: " . $mail->ErrorInfo;
+//         // echo '<span>'.$error.'</span>';
+//         header("location:index.php?msg=no");
+//       }
+//       else
+//       {
+//         header("location:index.php?msg=yes");
+//         // echo "Send Mail";
+//       }
     
 
 
